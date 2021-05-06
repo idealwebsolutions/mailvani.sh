b0c09e0eccf1b36dfdeaf7e85375deadee630935
idealwebsolutions <alex@idealwebsolutions.co>
2021-05-06 00:41:32 -0700

    adds request validation to make sure webhook is called from forwarder

--- pages/api/webhooks/forward.ts
+++ pages/api/webhooks/forward.ts
@@ -1,19 +1,31 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from 'next';
 
-/*import { reverse } from 'dns';
+import { reverse } from 'dns';
 import { promisify } from 'util';
 
-const reverseIp = promisify(reverse);*/
+const reverseIp = promisify(reverse);
 
-export default function handler (req: NextApiRequest, res: NextApiResponse) {
+// Webhook should only be called from forwarding server
+export default async function handler (req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     res.statusCode = 405;
     res.end();
-    return
+    return;
   }
-  console.log(req.headers)
-  console.log(req.body)
+  const connectingIp: string | string[] | undefined = req.headers['x-forwarded-for'] || req.headers['x-real-ip'];
+  let hostnames: string[] = [];
+  try {
+    hostnames = await reverseIp(connectingIp as string);
+  } catch (err) {
+    console.error(err);
+  }
+  if (!hostnames || !hostnames.some((hostname) => /forwardemail\.net$/g.test(hostname))) {
+    res.statusCode = 405;
+    res.end();
+    return;
+  }
+  console.log(req.body);
   res.statusCode = 200;
   res.end()
 }
